38974afe4e96390c7448080dadd4d8e894266ea0
tofayeLL <ahmedtofayel132@gmail.com>
2025-06-29 15:49:41 +0600

    added blog

---
+++ src/components/blog/blogHome.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const blogHome = () => {
+    return (
+        <section>
+            <h1>This is blog home page</h1>
+            
+        </section>
+    );
+};
+
+export default blogHome;
