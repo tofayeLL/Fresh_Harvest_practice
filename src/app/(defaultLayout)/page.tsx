d443942b22b7d367dcbee085bad81227f17eff95
tofayeLL <ahmedtofayel132@gmail.com>
2025-07-02 19:21:51 +0600

    added all the initial file

--- src/app/(defaultLayout)/page.tsx
+++ src/app/(defaultLayout)/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-5xl bg-red-400 mt-44">From home page</h1>
+      <h1 className="text-5xl bg-red-400 mt-44">This is home page</h1>
     </div>
   );
 };
